46aad9eb4f8b7096e2815a12a3fa53ac80fa90ab
aatishsharma28 <m1036115@mindtree.com>
2018-09-01 15:18:26 +0530

    Adding new files including client

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+   // mongoURI: 'mongodb://aatish28:aatish28@ds131041.mlab.com:31041/devconnector'
+   mongoURI: 'mongodb://localhost:27017/devconnector',
+   secretOrKey: 'secret@123'
+}
